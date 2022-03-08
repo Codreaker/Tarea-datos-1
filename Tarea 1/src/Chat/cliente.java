58ba9a80a030c5fa951bd6eff2d16b42ad7c87e0
Codreaker <85197498+Codreaker@users.noreply.github.com>
2022-03-08 17:10:30 -0600

    Add files via upload

---
+++ Tarea 1/src/Chat/cliente.java
@@ -0,0 +1,9 @@
+package Chat;
+
+import javax.swing.*;
+
+public class cliente {
+    private JTextArea msg_area;
+    private JButton msg_send;
+    private JTextField msg_text;
+}
---
+++ Tarea 1/src/Chat/servidor.java
@@ -0,0 +1,9 @@
+package Chat;
+
+import javax.swing.*;
+
+public class servidor {
+    private JTextArea msg_area;
+    private JButton msg_send;
+    private JTextField msg_text;
+}
