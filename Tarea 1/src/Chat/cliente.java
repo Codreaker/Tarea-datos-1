155a66f960c79e48df0caf1bee2e999a5306f21b
Codreaker <85197498+Codreaker@users.noreply.github.com>
2022-03-08 17:35:02 -0600

    Primer comit
    hola

--- Tarea 1/src/Chat/cliente.java
+++ Tarea 1/src/Chat/cliente.java
@@ -1,9 +1,16 @@
 package Chat;
 
-import javax.swing.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
 
-public class cliente {
-    private JTextArea msg_area;
-    private JButton msg_send;
-    private JTextField msg_text;
+
+public class cliente extends javax.swing.JFrame {
+    static Socket s;
+    static DataInputStream dis;
+    static DataOutputStream dout;
+    
+    public cliente() {
+        initComponents();
+    }
 }
