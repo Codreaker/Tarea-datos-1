c24fa1f16068d2f8235c702b3f2210fb1b3e57a7
Codreaker <85197498+Codreaker@users.noreply.github.com>
2022-03-10 14:38:33 -0600

    Funciona

--- Tarea 1/src/Chat/servidor.java
+++ Tarea 1/src/Chat/servidor.java
@@ -18,7 +18,7 @@ public class servidor extends javax.swing.JFrame {
     /* Creacion de la interfas grafica*/
 
 
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -33,7 +33,7 @@ public class servidor extends javax.swing.JFrame {
         msg_area.setRows(5);
         jScrollPane1.setViewportView(msg_area);
 
-        msg_send.setText("send");
+        msg_send.setText("Enviar");
         msg_send.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 msg_sendActionPerformed(evt);
@@ -41,7 +41,7 @@ public class servidor extends javax.swing.JFrame {
         });
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel1.setText("Server");
+        jLabel1.setText("Servidor");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
