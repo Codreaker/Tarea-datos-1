89a56e6df37754faac882e735f50dc3f5e091c9a
Codreaker <85197498+Codreaker@users.noreply.github.com>
2022-04-06 19:21:56 -0600

    Prueba exitosa]

--- Tarea2datos/src/main/java/com/mycompany/tarea2datos/main.java
+++ Tarea2datos/src/main/java/com/mycompany/tarea2datos/main.java
@@ -5,8 +5,9 @@ package com.mycompany.tarea2datos;
 public class main {
     
     public static void main(String[] args ){
-        Tarea2datos m = new Tarea2datos();
-        m.Escribir("Pruebas")        
+        Tarea2datos T = new Tarea2datos();
+        T.frameworks("Pruebas");
+        
     }
     
 }
